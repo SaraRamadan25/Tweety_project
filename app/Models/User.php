c4e9f83108bb68df54b2b28060ecd2e4e45110d2
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2022-09-10 13:16:18 +0200

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable,Followable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    public function getAvatarAttribute()
+    {
+        return "https://i.pravatar.cc/200?u=".$this->email;
+    }
+
+
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
+    public function timeline()
+    {
+        $friends = $this->follows()->pluck('id');
+
+        return Tweet::whereIn('user_id', $friends)
+            ->orWhere('user_id', $this->id)
+            ->withLikes()
+            ->orderByDesc('id')
+            ->paginate(50);
+    }
+
+    public function tweets()
+    {
+        return $this->hasMany(Tweet::class)->latest();
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function path($append = '')
+    {
+        $path = route('profile', $this->username);
+
+        return $append ? "{$path}/{$append}" : $path;
+    }
+
+}
+
+
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Http\Controllers\ExploreController;
+use App\Http\Controllers\FollowsController;
+use App\Http\Controllers\ProfilesController;
+use App\Http\Controllers\TweetLikesController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::middleware('auth')->group(function (){
+
+    Route::get('/tweets', [App\Http\Controllers\TweetsController::class, 'index'])->name('home');
+    Route::post('/tweets', [App\Http\Controllers\TweetsController::class, 'store']);
+
+    Route::post('/tweets/{tweet}/like',[TweetLikesController::class,'store']);
+    Route::delete('/tweets/{tweet}/like',[TweetLikesController::class,'destroy']);
+
+    Route::post('/profiles/{user:username}/follow',[FollowsController::class, 'store'])->name('follow');
+    Route::get('/profiles/{user:username}/edit',[ProfilesController::class, 'edit'])->middleware('can:edit,user');
+
+    Route::patch('/profiles/{user:username}',[ProfilesController::class, 'update'])->middleware('can:edit,user');
+
+    Route::get('/explore',[ExploreController::class,'index']);
+});
+
+Route::get('/profiles/{user:username}',[ProfilesController::class, 'show'])->name('profile');
+
+
+Auth::routes();
+
+
+
